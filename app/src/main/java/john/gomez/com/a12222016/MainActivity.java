9cd0d32d37afd2c4961898fcf90fd5150a0c1d80
lighterletter <jgomez818@gmail.com>
2016-12-21 10:11:06 -0500

    initial project commit

---
+++ app/src/main/java/john/gomez/com/a12222016/MainActivity.java
@@ -0,0 +1,13 @@
+package john.gomez.com.a12222016;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
